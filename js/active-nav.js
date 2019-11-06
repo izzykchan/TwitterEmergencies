de9175d04cd90a333a38515d8527a02fab811155
Preston <promey@sfu.ca>
2019-11-05 22:00:01 -0800

    fixed the call to JQueries, added more change notes

--- js/active-nav.js
+++ js/active-nav.js
@@ -1,3 +1,4 @@
+ 
 $(window).scroll(function() {
 		var scrollDistance = $(window).scrollTop();
 
@@ -16,3 +17,26 @@ $(window).scroll(function() {
 				}
 		});
 }).scroll();
+
+
+//ALTERNATE SMOOTH SCROLL
+
+//only loads when page is ready
+$(document).ready(function(){
+	var scrollLink = $('.scroll');
+
+	// Smooth scrolling
+	scrollLink.click(function(event){
+		event.preventDefaul();
+		$('body,html').animate({scrollTop: $(this.hash).offset().top}, 1000 ) //scroll top references how far down the scroll bar is
+
+	})
+
+})
+
+//ACTIVE LINK SWITCHING
+$(window).scroll(function(){
+	var scrollbarLocation = $(this).scrollTop();
+
+	console.log(scrollbarLocation)
+})
