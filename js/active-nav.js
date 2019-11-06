f0ab7a0e4bc8dad47e0fc3ebdf7c283d4dbefef2
Preston <promey@sfu.ca>
2019-11-06 15:15:20 -0800

    Scroll changes active text, hamburger menu works,
    fixed some small alignment bugs as well

--- js/active-nav.js
+++ js/active-nav.js
@@ -1,4 +1,21 @@
  
+ //SMOOTH SCROLL
+
+//only loads when page is ready
+$(document).ready(function(){
+	var scrollLink = $('.scroll');
+
+	// Smooth scrolling
+	scrollLink.click(function(event){
+		//event.preventDefault();
+		$('body,html').animate({scrollTop: $(this.hash).offset().top}, 100 ) //scroll top references how far down the scroll bar is
+
+	})
+
+})
+
+//ACTIVATE / DEACTIVATE NAVIGATION BUTTONS THROUGH SCROLL
+
 $(window).scroll(function() {
 		var scrollDistance = $(window).scrollTop();
 
@@ -11,30 +28,18 @@ $(window).scroll(function() {
 
 		// Assign active class to nav links while scolling
 		$('.page-section').each(function(i) {
-				if ($(this).position().top <= scrollDistance) {
+				if ($(this).position().top <= scrollDistance+50) {
 						$('#navBar a.active').removeClass('active');
+						//console.log("deactivated")
 						$('#navBar a').eq(i).addClass('active');
 				}
 		});
 }).scroll();
 
 
-//ALTERNATE SMOOTH SCROLL
 
-//only loads when page is ready
-$(document).ready(function(){
-	var scrollLink = $('.scroll');
 
-	// Smooth scrolling
-	scrollLink.click(function(event){
-		event.preventDefaul();
-		$('body,html').animate({scrollTop: $(this.hash).offset().top}, 1000 ) //scroll top references how far down the scroll bar is
-
-	})
-
-})
-
-//ACTIVE LINK SWITCHING
+//ACTIVE LINK SWITCHING (Doesnt work)
 $(window).scroll(function(){
 	var scrollbarLocation = $(this).scrollTop();
 
